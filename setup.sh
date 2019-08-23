fe3cd0050bb5275489673177210bd6c13ad9389f
Micah Stubbs <micah.stubbs@gmail.com>
2019-08-22 22:19:21 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -7,5 +7,15 @@ source activate opengpt2
 # install jupyter lab
 conda install -c conda-forge jupyterlab
 
+# install some dependencies
+
+# follow the relevant guide to setup tensorflow-gpu a machine with an nVidia graphics card:
+
+# macOS
+# https://gist.github.com/ageitgey/819a51afa4613649bd18
+
+# Windows 10 
+# https://towardsdatascience.com/installing-tensorflow-with-cuda-cudnn-and-gpu-support-on-windows-10-60693e46e781
+
 # open jupyter lab
 jupyter lab
