ac9002778d4954927bb8ba1767bfcbde381040fa
Micah Stubbs <micah.stubbs@gmail.com>
2019-08-22 22:39:43 -0700

    document that nvidia-cuda doesn't work on macOS 10.14

--- setup.sh
+++ setup.sh
@@ -11,11 +11,23 @@ conda install -c conda-forge jupyterlab
 
 # follow the relevant guide to setup tensorflow-gpu a machine with an nVidia graphics card:
 
-# macOS
+#
+# !Windows 10 
+#
+# https://towardsdatascience.com/installing-tensorflow-with-cuda-cudnn-and-gpu-support-on-windows-10-60693e46e781
+
+#
+# !macOS
+#
 # https://gist.github.com/ageitgey/819a51afa4613649bd18
 
-# Windows 10 
-# https://towardsdatascience.com/installing-tensorflow-with-cuda-cudnn-and-gpu-support-on-windows-10-60693e46e781
+# !modifications
+# brew tap caskroom/drivers
+# brew cask install nvidia-cuda
+
+# nvm, won't work on Mac 10.14 :facepalm:
+# Error: Cask nvidia-cuda depends on macOS release being one of [10.12, 10.13], but you are running release 10.14.
+
 
 # open jupyter lab
 jupyter lab
